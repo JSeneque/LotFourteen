08e1b9b3a3a7ab89e75d5b98d8a913b3a9f98f59
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2020-10-05 07:47:35 +1030

    Main Menu and tidy up before submission

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<bool> goals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goals = new List<bool>(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
